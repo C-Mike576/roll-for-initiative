bcfa1d56560b7048b4550eb4814ddcc85ba58eb7
c-mike576 <m.calkins576@gmail.com>
2019-12-05 16:20:24 -0500

    moved exe

--- lib/roll_for_initiative.rb
+++ lib/roll_for_initiative.rb
@@ -2,8 +2,9 @@ require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
-
 require_relative "./roll_for_initiative/version"
 require_relative './roll_for_initiative/cli'
 require_relative './roll_for_initiative/klass'
 require_relative './roll_for_initiative/scraper'
+
+RollForInitiative::CLI.new.call
